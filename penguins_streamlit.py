ade41f0cbbfed30ac132129ac92bf46229779232
vibhorgupta31 <vibhorgupta7@gmail.com>
2022-07-18 18:17:34 -0400

    test st.secrets()

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -9,6 +9,8 @@ st.write("This app uses 6 inputs to predict the species of penguin using"
 	"a model built on the Palmer's Penguin's dataset. Use the form below"
 	" to get started!")
 
+st.write(st.secrets["password"])
+
 password_guess = st.text_input('What is the Password?')
 if password_guess != st.secrets["password"]:
 	st.stop()
